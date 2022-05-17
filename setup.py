df2719559a65a76883a0d7ec9e16ee7c673cc3b0
alex_malme <60219106+alexmalme@users.noreply.github.com>
2022-05-17 18:52:31 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-
-# Learn more: https://github.com/kennethreitz/setup.py
-
 from setuptools import setup, find_packages
 
 
@@ -12,13 +9,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='sample etl',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='Sample ETL using Prefect',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Alexandre Mello',
+    author_email='alexandre@malme.com.br',
+    url='https://github.com/alexmalme/etl_w',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
